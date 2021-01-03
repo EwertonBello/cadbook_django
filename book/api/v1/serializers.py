a633c4b806e64457a0480b4ff77d819a78200d3f
EwertonBello <ewerton.bello1212@gmail.com>
2021-01-03 00:05:52 -0300

    finish api for books

--- book/api/v1/serializers.py
+++ book/api/v1/serializers.py
@@ -1,8 +1,21 @@
+from django.contrib.auth.models import User
 from book.models import Book
 from rest_framework import serializers
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'username',
+            'email',
+        ]
+
+
 class BookSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+
     class Meta:
         model = Book
         fields = [
--- book/urls.py
+++ book/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "book"
 
 urlpatterns = [
-	path("api/v1/", include('book.api.v1')),
+	path("api/v1/", include('book.api.v1.urls')),
 	path("", views.book_dashboard, name="dashboard"),
 	path("books/", views.book_list, name="list"),
 	path("books/<int:book_id>/", views.book_detail, name="detail"),
