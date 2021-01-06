cfe3953eb3c4e869b9152e79b6e1b366eba92357
EwertonBello <ewerton.bello1212@gmail.com>
2021-01-06 16:14:47 -0300

    create and change model user

--- cadbook/settings/base.py
+++ cadbook/settings/base.py
@@ -77,7 +77,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'cadbook.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -91,7 +90,6 @@ DATABASES = {
         "PORT": 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -111,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -124,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
